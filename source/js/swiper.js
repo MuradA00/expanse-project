6abf2858346b0e61e4eadd642c10ab9202504c14
Murad <abishlimurad@gmail.com>
2023-01-25 21:01:17 +0200

    update section 5%

--- source/js/swiper.js
+++ source/js/swiper.js
@@ -1,4 +1,9 @@
 const newsRow = new Swiper('.news__wrapper', {
   slidesPerView: 'auto',
-  spaceBetween: 65
+  spaceBetween: 20,
+  breakpoints: {
+    1200: {
+      spaceBetween: 65
+    }
+  }
 })
